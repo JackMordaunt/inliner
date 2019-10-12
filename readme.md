bdceebe04ed024075d3d8ee79696231ff9b6fc5c
= <=>
2019-10-12 15:26:55 +1300

    [+] Inline media files using data urls. Use `clap` for cli.

--- readme.md
+++ readme.md
@@ -2,9 +2,15 @@
 
 > Create a self contained html file that can be displayed correctly in a browser.
 
-- [x] Parse valid html text input into a tree structure.
-- [ ] Replace all links with the content of the linked resource.
-- [ ] Render new html with inlined content.
+`inliner` is expected to be used with a build script that produces optimized
+output, such as `yarn build`. Optimizing `css` and `js` and fetching remote
+resources is outside the scope of this library - at least for now.
 
-Data structure only needs to be able to do basic html manipulation, and contain
-enough data to correctly render out the html document as text.
+## Features
+
+- [x] Parse valid html text input into a tree structure.
+- [x] Replace all links with the content of the corresponding file from disk.
+- [x] Render new html with inlined content.
+
+Text files are embedded directly.
+Media files are embedded as base64 encoded data urls.
--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,55 @@
 mod html;
 
+use clap::{App, Arg};
 use html::{Node, NodeRef, Parser, Tokenizer};
+use std::error::Error;
 use std::fs;
 use std::io::prelude::*;
+use std::io::BufReader;
+use std::path::Path;
 
 fn main() {
-    let input_file = match std::env::args().nth(1).take() {
-        Some(path) => path,
-        None => {
-            eprintln!("please provide path to input file");
-            return;
-        }
-    };
-    let base_dir = match std::env::args().nth(2).take() {
-        Some(path) => path,
-        None => ".".to_string(),
-    };
-    let mut input = String::new();
-    match fs::File::open(input_file) {
-        Ok(mut file) => {
-            if let Err(err) = file.read_to_string(&mut input) {
-                eprintln!("error: reading input file: {}", err);
-                return;
-            }
-        }
+    let cli = App::new("inliner")
+        .author("Jack Mordaunt <jackmordaunt@gmail.com>")
+        .about("Take html resources and bundle them into a single html file.")
+        .arg(
+            Arg::with_name("input")
+                .required(true)
+                .takes_value(true)
+                .help("Path to html file"),
+        )
+        .arg(
+            Arg::with_name("base")
+                .required(false)
+                .takes_value(true)
+                .default_value(".")
+                .help("Directory which links will be resolved against"),
+        )
+        .get_matches();
+    let input = match fs::read_to_string(cli.value_of("input").unwrap()) {
+        Ok(content) => content,
         Err(err) => {
             eprintln!("error: opening input file: {}", err);
             return;
         }
     };
+    let inlined = match inline(input, cli.value_of("base").unwrap().as_ref()) {
+        Ok(output) => output,
+        Err(err) => {
+            eprintln!("error: inlining html: {}", err);
+            return;
+        }
+    };
+    if let Err(err) = std::io::stdout().write_all(inlined.as_bytes()) {
+        eprintln!("error: writing to stdout: {}", err);
+        return;
+    };
+}
+
+// Inline html resources into a single html buffer. Consumes input.
+// Media files are base64 encoded in data urls, text files are directly
+// embedded.
+fn inline(mut input: String, base: &Path) -> Result<String, Box<dyn Error>> {
     let dom = Parser::new(Tokenizer::new(input.drain(..)).merged())
         .parse()
         .expect("parsing dom");
@@ -40,41 +61,55 @@ fn main() {
         } = &mut *n.borrow_mut()
         {
             let attr = attributes;
-            // Todo: embed manifest file with data url.
-            if attr
-                .get("rel")
-                .and_then(|rel| Some(rel == "manifest"))
-                .unwrap_or(false)
-            {
-                return Ok(());
-            }
             if let Some(link) = attr.get("href").or(attr.get("src")) {
-                use std::cell::RefCell;
-                use std::path::PathBuf;
-                use std::rc::Rc;
                 let link = link.trim_matches('/');
-                // Todo: Embed images via base64 encoded data urls.
-                if link.ends_with("png") || link.ends_with("ico") || link.ends_with("jpeg") {
-                    return Ok(());
-                }
-                let path = PathBuf::from(base_dir.clone()).join(link);
-                let content = fs::read_to_string(&path)
-                    .map_err(|e| format!("{}: {:?}", &path.to_string_lossy(), e))?;
-                if link.ends_with("css") {
-                    *name = "style".to_string();
-                    attr.remove("rel");
-                }
-                attr.remove("href");
-                attr.remove("src");
-                children.clear();
-                children.push(Rc::new(RefCell::new(Node::Text(content))));
+                let path = base.join(link);
+                let is_plain_text = ["html", "js", "css"].into_iter().fold(false, |acc, ext| {
+                    if acc {
+                        true
+                    } else {
+                        link.ends_with(ext)
+                    }
+                });
+                match is_plain_text {
+                    false => {
+                        let file = fs::File::open(&path)
+                            .map_err(|e| format!("{}: {:?}", &path.to_string_lossy(), e))?;
+                        let content = base64::encode(
+                            BufReader::new(file)
+                                .bytes()
+                                .map(Result::ok)
+                                .filter_map(|b| b)
+                                .collect::<Vec<u8>>()
+                                .as_slice(),
+                        );
+                        let data_url = format!(
+                            "data:{media_type};bas64,{data}",
+                            media_type = mime_guess::from_path(&link).first_or_octet_stream(),
+                            data = content
+                        );
+                        if attr.contains_key("href") {
+                            attr.insert("href".into(), data_url);
+                        } else if attr.contains_key("src") {
+                            attr.insert("src".into(), data_url);
+                        }
+                    }
+                    true => {
+                        let content = fs::read_to_string(&path)
+                            .map_err(|e| format!("{}: {:?}", &path.to_string_lossy(), e))?;
+                        if link.ends_with("css") {
+                            *name = "style".to_string();
+                            attr.remove("rel");
+                        }
+                        attr.remove("href");
+                        attr.remove("src");
+                        children.clear();
+                        children.push(Node::Text(content).into());
+                    }
+                };
             }
         }
         Ok(())
-    })
-    .expect("inlining files");
-    if let Err(err) = std::io::stdout().write_all(dom.to_string().as_bytes()) {
-        eprintln!("error: writing to stdout: {}", err);
-        return;
-    };
+    })?;
+    Ok(dom.to_string())
 }
